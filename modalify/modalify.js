df0c5eb7652b57ec61a45487bc9bce0e7a5058a5
Dan Pratt <dpratt@innov8d.com>
2011-12-13 14:02:19 -0500

    * Fixed problems with multiple modals on one page  * Fixed centering issue when content was longer than one page  * Fixed sizing/placement issues across multiple browsers  * Fixed problem with close buttons not getting click events  * Fixed problem with close button getting under the mask

--- modalify/modalify.js
+++ modalify/modalify.js
@@ -50,6 +50,8 @@
     function init(options) {
         var activeOptions = $.extend({
 	            maskClass: "overlay",
+	            maskOpenClass: "overlay-open",
+	            modalOpenClass: "modal-open",
 	            closeClass: "modal-close",
 	            outerWrapperClass: "outer",
 	            innerWrapperClass: "inner",
@@ -65,8 +67,9 @@
             $("body")
             	.append(mask)
 	            .keydown(onCancel);
-	            
-            $(window).resize(onResize);
+
+            $(window)
+            	.resize(onResize);
             
             _initialized = true;
         }
@@ -75,53 +78,76 @@
 
         function modalize(i, content) {
             var $content = $(content).wrap("<div class=\"" + activeOptions.outerWrapperClass + "\">" 
-            		+ "<div class=\"" + activeOptions.innerWrapperClass + "\" />" 
+            		+ "<div class=\"" + activeOptions.innerWrapperClass + "\"></div>" 
             		+ "<a href=\"#\" class=\"" + activeOptions.closeClass + "\">X</a></div>"),
                 $modal = $content.parent().parent();
                 $closeLink = $modal.find("." + activeOptions.closeClass);
             
+        	$content.show();
+			activeOptions.$modal = $modal;
+			activeOptions.$mask = $("." + activeOptions.maskClass);
+			activeOptions.$closeLink = $closeLink;
             $("body").append($modal);
-            $content.data("modalify", activeOptions);
+            $content.data("modalify", $.extend({}, activeOptions));
+            $content.modalify("center");
         }
     }
 
     function center() {
         var $content = $(this),
-            $modal = $content.parent().parent(),
-            options = $content.data("modalify");
+            options = $content.data("modalify"),
+            startHeight = options.$modal.height(),
+            startWidth = options.$modal.width()
+            isVisible = options.$modal.filter(":visible").length != 0;
 
-        $modal.css("height", "");
+		if (!isVisible) {
+			options.$modal.css("zoom", "1");
+			options.$modal.css("filter", "alpha(opacity=0)");
+		    options.$modal.css("opacity", "0");
+			options.$modal.css("display", "block");
+		}
 
-        if ($content.height() <= ($modal.height() - 2 * options.contentPadding)) {
-            $modal.css("height", ($content.height() + 2 * options.contentPadding) + "px");
+        options.$modal.css("height", "");
+
+        if ($content.height() <= (options.$modal.height() - 2 * options.contentPadding)) {
+            options.$modal.css("height", ($content.height() + 2 * options.contentPadding) + "px");
         }
 
-        $modal.position({
-            of: $("body"),
+        options.$modal.position({
+            of: $(window),
             my: "center center",
             at: "center center"
         });
+
+        if (options && options.onResize && ((startHeight != options.$modal.height()) || (startWidth != options.$modal.width()))) {
+            options.onResize($content, $content.width(), $content.height());
+        }
+
+		if (!isVisible) {
+			options.$modal.css("zoom", "");
+			options.$modal.css("filter", "");
+		    options.$modal.css("opacity", "");
+			options.$modal.css("display", "");
+		}
     }
 
     function open() {
         close();
-        
+
         _activeModal = $(this);
         var options = _activeModal.data("modalify");
-        $closeLink.click(onMaskClick);
-        
-        $("." + options.maskClass).show();
-        _activeModal.show();
-        _activeModal.parent().parent().show();
+        options.$closeLink.click(onMaskClick);
+        options.$mask.addClass(options.maskOpenClass);
+        options.$modal.addClass(options.modalOpenClass);
         _activeModal.modalify("center");
-
     }
 
     function close() {
         if (_activeModal) {
             var options = _activeModal.data("modalify");
-            $("." + options.maskClass).hide();
-            _activeModal.parent().parent().hide();
+			options.$closeLink.unbind("click", onMaskClick);
+        	options.$mask.removeClass(options.maskOpenClass);
+        	options.$modal.removeClass(options.modalOpenClass);
             _activeModal = null;
         }
     }
@@ -162,10 +188,6 @@
             var options = _activeModal.data("modalify");
 
             _activeModal.modalify("center");
-
-            if (options && options.onResize) {
-                options.onResize(_activeModal, _activeModal.width(), _activeModal.height());
-            }
         }
     }
 })();
